84efe05e37af093a1fdbfcb0d32386112e1f06db
GilHernandez7 <gihernandezdo@gmail.com>
2021-08-31 12:12:50 -0500

    Basketball Minigame
    Basket minigame componets:
    * Controllers
    * Minigame Script
    * Scene

---
+++ Assets/Scripts/Minigames/BasketBallMiniGame.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Voidless;
+using System;
+
+
+namespace Flamingo
+{
+   
+[Serializable]    
+public class BasketBallMiniGame : MiniGame
+{
+
+        [SerializeField] private Ring blueBasket;                                /// <summary>Basket for blue player.</summary>
+        [SerializeField]private Ring redBasket;                                 /// <summary>Basket for red player.</summary>
+        private int blueScore;                                                  /// <summary>Blue player score.</summary>
+        private int redScore;                                                   /// <summary>Red player score.</summary>
+        [Header("Time Variables")]
+        [SerializeField] public Clock basketClock;                              /// <summary>Clock class for the minigame.</summary>
+        [SerializeField] private float _limitMinutesAmount;                     /// <summary>Limit time for the minigame.</summary>
+
+        [Header("UI")]
+        [SerializeField] private TextMesh _timeText;                            /// <summary>Text mesh to show the time left of the minigame.</summary>
+        [SerializeField] private TextMesh _blueScoreText;                       /// <summary>Text mesh to show the score for blue player.</summary>
+        [SerializeField] private TextMesh _redScoreText;                        /// <summary>Text mesh to show the score for red player.</summary>
+        private string _winnerName;
+
+
+
+#region Set/Get
+
+
+        /// <summary>Gets and Sets BlueBasket property.</summary>
+        public Ring BlueBasket
+        {
+            get { return blueBasket; }
+            set { blueBasket = value; }
+        }
+        /// <summary>Gets and Sets RedBasket property.</summary>
+        public Ring RedBasket
+        {
+            get { return redBasket; }
+            set { redBasket = value; }
+        }
+        /// <summary>Gets and Sets BlueScore property.</summary>
+        public int BlueScore
+        {
+            get { return blueScore; }
+            set {blueScore = value; }
+        }
+        /// <summary>Gets and Sets RedScore property.</summary>
+        public int RedScore
+        {
+            get { return redScore; }
+            set { redScore = value; }
+        }
+        /// <summary>Gets and Sets limitMinutes for minigame property.</summary>
+        public float limitMinutesAmount
+        {
+            get { return _limitMinutesAmount; }
+            set { _limitMinutesAmount = value; }
+        }
+        /// <summary>Gets and Sets Timer Text property.</summary>
+        public TextMesh timeText
+        {
+            set { _timeText = value; }
+            get { return _timeText; }
+        }
+        /// <summary>Gets and Sets blueScore text property.</summary>
+        public TextMesh blueScoreText
+        {
+            set { _blueScoreText = value; }
+            get { return _blueScoreText; }
+        }
+        /// <summary>Gets and Sets redScore text property.</summary>
+        public TextMesh redScoreText
+        {
+            set { _redScoreText = value; }
+            get { return _redScoreText; }
+        }
+#endregion
+
+
+
+#region Events Callbacks  
+        /// <summary>Callback for onRingPassed's Event if its the red basket.</summary>
+        /// <param name="_collider">Collider that passed the ring.</param>
+        public void OnRedRingPassed(Collider2D _collider)
+        {
+           
+            blueScore++;
+            blueScoreText.text = blueScore.ToString();
+            Debug.Log("BLUE MAKES A SCORE BABYYY : " + blueScore);
+
+        }
+        /// <summary>Callback for onRingPassed's Event if its the blue basket.</summary>
+        /// <param name="_collider">Collider that passed the ring.</param>
+        public void OnBlueRingPassed(Collider2D _collider)
+        {
+            redScore++;
+            redScoreText.text = redScore.ToString();
+            Debug.Log("RED MAKES A SCORE BABYYY : " + redScore);
+
+        }
+#endregion
+
+        /// <summary>Basket ball Mini-Game's Coroutine.</summary>
+        
+        protected override IEnumerator MiniGameCoroutine()
+        {
+            redBasket.onRingPassed += OnRedRingPassed;
+            blueBasket.onRingPassed += OnBlueRingPassed;
+      
+            while (basketClock.minutes < limitMinutesAmount)
+            {
+                yield return new WaitForEndOfFrame();
+                basketClock.Update(Time.deltaTime);
+                timeText.text = basketClock.ToString();
+            }    
+            timeText.text = "Game Over \n" + ReturnWinner()  ;
+
+
+            //Desuscribirse
+            redBasket.onRingPassed -= OnRedRingPassed;
+            blueBasket.onRingPassed -= OnBlueRingPassed;
+           
+
+            yield return null;
+           
+        }
+
+   
+private string ReturnWinner()
+        {
+            if(redScore>blueScore)
+            {
+                _winnerName = "Red player wins!";
+            }
+            else if(blueScore>redScore)
+            {
+                _winnerName = "Blue player wins!";
+            }
+            else if (redScore == blueScore)
+            {
+                _winnerName = "TIE!";
+            }
+            return _winnerName;
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Minigames/Controllers/BaskatBallMiniGameController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Flamingo {
+
+    public class BaskatBallMiniGameController : MonoBehaviour
+    {
+        [SerializeField] private BasketBallMiniGame _basketMiniGame;                    /// <summary> Reference to BasketMiniGame that initialize the minigame       
+        [SerializeField] private Vector3 _ballInitialPos;                               /// <summary>Spawn Position to reset the ball on ring passed.</summary>
+        [SerializeField] private GameObject _ball;                                      /// <summary>References to the ball.</summary>
+
+
+
+#region Getters/Setters
+
+        /// <summary>Gets and Sets basketMiniGame property.</summary>
+        public BasketBallMiniGame basketMiniGame { get { return _basketMiniGame; } }
+
+        /// <summary>Get and Sets ballInitialPos property.</summary>
+        public Vector3 ballInitialPos
+        {
+            set { _ballInitialPos = value; }
+            get { return _ballInitialPos; }
+        }
+
+        /// <summary>Get and Set ball property.</summary>
+        public GameObject ball
+        {
+            set { _ball = value; }
+            get { return _ball; }
+        }
+
+
+#endregion
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(ballInitialPos, .5f);
+        }
+#region Unity Calls
+        private void Awake()
+        {
+            basketMiniGame.Initialize(this);
+            
+            
+            SubscribeBall();
+        }
+
+        private void OnDestroy()
+        {
+            basketMiniGame.Terminate(this);
+            Unsubscribe();   
+        }
+        #endregion
+
+        #region Functions
+
+        /// <summary>Function to subscribe ResetBall event to rings.</summary>
+        void SubscribeBall()
+        {
+
+            basketMiniGame.RedBasket.onRingPassed += ResetBallOnRingPassed;
+            basketMiniGame.BlueBasket.onRingPassed += ResetBallOnRingPassed;
+        }
+
+        void Unsubscribe()
+        {
+           /* basketMiniGame.RedBasket.onRingPassed -= basketMiniGame.OnRedRingPassed;
+            basketMiniGame.BlueBasket.onRingPassed -= basketMiniGame.OnBlueRingPassed;*/
+            basketMiniGame.RedBasket.onRingPassed -= ResetBallOnRingPassed;
+            basketMiniGame.BlueBasket.onRingPassed -= ResetBallOnRingPassed;
+        }
+
+        /// <summary>Override delegate for OnRingedPassed .</summary>
+        public void ResetBallOnRingPassed(Collider2D _collider)
+        {
+            ball.GetComponent<Rigidbody2D>().Sleep();
+            ball.transform.position = ballInitialPos;
+        }
+
+      
+#endregion
+
+    }
+
+}
