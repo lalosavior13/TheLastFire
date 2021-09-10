a2616ea8d1fd7ce8225274adc8db0729cef8dabe
GilHernandez7 <gihernandezdo@gmail.com>
2021-09-09 20:05:18 -0500

    Ring Madness Minigame
    Create the scene and componets that are needed to play RingsMadness

---
+++ Assets/Scripts/Minigames/Controllers/RingMadnessController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Voidless;
+
+namespace Flamingo
+{
+    public class RingMadnessController : MonoBehaviour
+    {
+        [SerializeField] private RingMadness_Minigame _ringMiniGame;
+        
+
+        // Start is called before the first frame update
+        public RingMadness_Minigame ringMinigame { get { return _ringMiniGame; } }
+        private void Awake()
+        {
+            ringMinigame.Initialize(this);
+        }
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        
+    }
+}
