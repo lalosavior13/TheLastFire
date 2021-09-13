28d5b978281ff04fb401d13c85b6904e4617176f
lalosavior13 <lalosavior13@gmail.com>
2021-09-13 17:25:15 -0500

    NavigationScript & Build Settings

---
+++ Assets/Scripts/Navigation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Voidless;
+using Flamingo;
+
+
+
+public class Navigation : MonoBehaviour
+{
+    [SerializeField] private Button[] buttons;
+
+    void Start()
+    {
+        InitializeButtons();
+    }
+
+    private void InitializeButtons()
+    {
+        
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            Button button = buttons[i];
+
+            int buttonIndex = i;
+            Debug.Log("Button initialized " + i);
+        }
+    }
+
+    public void ChangeScene(string _sceneName)
+    {
+        SceneManager.LoadScene(_sceneName);
+        
+    }
+    public void Button0(int value)
+    {
+        Debug.Log("Button " + value + " Pressed");
+    }
+}
